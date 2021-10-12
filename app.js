0ef44df548f5040b5e6e4ef6ca58be6ffeae12ba
Alexis Goup <alexis.goupillot.dev@gmail.com>
2021-10-12 16:10:29 +0200

    added bodyParser

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const api = require('./api/v1/index');
 const cors = require('cors');
 
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const connection = mongoose.connection;
 
 app.set('port', (process.env.port || 3000)); // Configuration du Port
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
 	console.log(`Request handled at ${new Date()}`);
 	next();
